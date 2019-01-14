6625ac78c5a9a75f2e70e6894150613a42c2b70e
andrewgsavage <andrewgsavage@gmail.com>
2019-01-14 22:38:11 +0000

    add searchsorted

--- pintpandas/pint_array.py
+++ pintpandas/pint_array.py
@@ -623,6 +623,57 @@ class PintArray(ExtensionArray, ExtensionOpsMixin):
         qtys = [item * self.units for item in self._data]
         return np.array(qtys, dtype = "object", copy = copy)
     
+    def searchsorted(self, value, side="left", sorter=None):
+        """
+        Find indices where elements should be inserted to maintain order.
+
+        .. versionadded:: 0.24.0
+
+        Find the indices into a sorted array `self` (a) such that, if the
+        corresponding elements in `v` were inserted before the indices, the
+        order of `self` would be preserved.
+
+        Assuming that `a` is sorted:
+
+        ======  ============================
+        `side`  returned index `i` satisfies
+        ======  ============================
+        left    ``self[i-1] < v <= self[i]``
+        right   ``self[i-1] <= v < self[i]``
+        ======  ============================
+
+        Parameters
+        ----------
+        value : array_like
+            Values to insert into `self`.
+        side : {'left', 'right'}, optional
+            If 'left', the index of the first suitable location found is given.
+            If 'right', return the last such index.  If there is no suitable
+            index, return either 0 or N (where N is the length of `self`).
+        sorter : 1-D array_like, optional
+            Optional array of integer indices that sort array a into ascending
+            order. They are typically the result of argsort.
+
+        Returns
+        -------
+        indices : array of ints
+            Array of insertion points with the same shape as `value`.
+
+        See Also
+        --------
+        numpy.searchsorted : Similar method from NumPy.
+        """
+        # Note: the base tests provided by pandas only test the basics.
+        # We do not test
+        # 1. Values outside the range of the `data_for_sorting` fixture
+        # 2. Values between the values in the `data_for_sorting` fixture
+        # 3. Missing values.
+        arr = self._data
+        if isinstance(value,_Quantity):
+            value = value.to(self.units).magnitude
+        elif is_list_like(value) and isinstance(value[0],_Quantity):
+            value = [item.to(self.units).magnitude for item in value]
+        return arr.searchsorted(value, side=side, sorter=sorter)
 PintArray._add_arithmetic_ops()
 PintArray._add_comparison_ops()
 register_extension_dtype(PintType)
