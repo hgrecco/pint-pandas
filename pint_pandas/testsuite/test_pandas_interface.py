3c69988c82130d2b9351666b6df36867b7273054
andrewgsavage <andrewgsavage@gmail.com>
2020-10-11 00:14:59 +0100

    lint

--- pint_pandas/testsuite/test_pandas_interface.py
+++ pint_pandas/testsuite/test_pandas_interface.py
@@ -49,7 +49,9 @@ def data_missing():
 
 @pytest.fixture
 def data_for_twos():
-    x = [2,] * 100
+    x = [
+        2,
+    ] * 100
     return ppi.PintArray.from_1darray_quantity(x * ureg.meter)
 
 
@@ -88,8 +90,7 @@ def data_missing_for_sorting():
 
 @pytest.fixture
 def na_cmp():
-    """Binary operator for comparing NA values.
-    """
+    """Binary operator for comparing NA values."""
     return lambda x, y: bool(np.isnan(x.magnitude)) & bool(np.isnan(y.magnitude))
 
 
@@ -692,7 +693,10 @@ class TestDataFrameAccessor(object):
             names=["Car type", "metric", "unit"],
         )
         df.index = pd.MultiIndex.from_arrays(
-            [[1, 12, 32, 48], ["Tim", "Tim", "Jane", "Steve"],],  # noqa E231
+            [
+                [1, 12, 32, 48],
+                ["Tim", "Tim", "Jane", "Steve"],
+            ],  # noqa E231
             names=["Measurement number", "Measurer"],
         )
 
