1da01cfb6714453dc894f6849a2269f64c4d0199
andrewgsavage <andrewgsavage@gmail.com>
2021-02-27 14:08:21 +0000

    use default ureg

--- pint_pandas/pint_array.py
+++ pint_pandas/pint_array.py
@@ -34,7 +34,7 @@ class PintType(ExtensionDtype):
     _metadata = ("units",)
     _match = re.compile(r"(P|p)int\[(?P<units>.+)\]")
     _cache = {}
-    ureg = pint.UnitRegistry()
+    ureg = pint.Quantity(1,"m")._REGISTRY
 
     @property
     def _is_numeric(self):
