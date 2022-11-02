5f667a3994e5cdd28c8d5062759786cbd19a3e6e
Egil <egil.moller@freecode.no>
2022-11-02 19:35:30 +0100

    Got all tests to pass / be excluded where applicable with complex128

--- pint_pandas/testsuite/test_pandas_extensiontests.py
+++ pint_pandas/testsuite/test_pandas_extensiontests.py
@@ -42,7 +42,8 @@ def dtype():
     return PintType("pint[meter]")
 
 
-_all_numeric_dtypes = [float, int, np.complex128]
+_base_numeric_dtypes = [float, int]
+_all_numeric_dtypes = _base_numeric_dtypes + [np.complex128]
 
 @pytest.fixture(params=_all_numeric_dtypes)
 def data(request):
@@ -371,6 +372,10 @@ class TestArithmeticOps(base.BaseArithmeticOpsTests):
 
         return op_name, None
 
+    @pytest.mark.parametrize('data', _base_numeric_dtypes, indirect=True)
+    def test_divmod_series_array(self, data, data_for_twos):
+        base.BaseArithmeticOpsTests.test_divmod_series_array(self, data, data_for_twos)
+        
     def test_arith_series_with_scalar(self, data, all_arithmetic_operators):
         # series & scalar
         op_name, exc = self._get_exception(data, all_arithmetic_operators)
@@ -390,6 +395,7 @@ class TestArithmeticOps(base.BaseArithmeticOpsTests):
         self.check_opname(df, op_name, data[0], exc=exc)
 
     # parameterise this to try divisor not equal to 1
+    @pytest.mark.parametrize('data', _base_numeric_dtypes, indirect=True)
     def test_divmod(self, data):
         s = pd.Series(data)
         self._check_divmod_op(s, divmod, 1 * ureg.Mm)
@@ -462,7 +468,7 @@ class TestComparisonOps(base.BaseComparisonOpsTests):
 class TestOpsUtil(base.BaseOpsUtil):
     pass
 
-
+@pytest.mark.parametrize('data', _base_numeric_dtypes, indirect=True)
 class TestParsing(base.BaseParsingTests):
     pass
 
@@ -587,6 +593,10 @@ class TestReshaping(base.BaseReshapingTests):
 
 
 class TestSetitem(base.BaseSetitemTests):
+    @pytest.mark.parametrize('data', _base_numeric_dtypes, indirect=True)
+    def test_setitem_scalar_key_sequence_raise(self, data):
+        base.BaseSetitemTests.test_setitem_scalar_key_sequence_raise(self, data)
+        
     # @pytest.mark.xfail(run=True, reason="excess warnings, needs debugging")
     def test_setitem_frame_2d_values(self, data):
         # GH#44514
