c37b0e0e24f11c852a222086d11764bd9137aed5
Andrew <andrewgsavage@gmail.com>
2023-06-27 22:46:23 +0100

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,4 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "_static/logo-full.jpg"
-
-html_static_path = ["_static"]
 html_css_files = ["style.css"]
