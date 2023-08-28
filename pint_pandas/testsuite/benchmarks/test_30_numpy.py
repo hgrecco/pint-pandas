d15b25ec43972cebec629a07f923987552bb0cd1
Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>
2023-08-28 13:05:48 -0400

    Initial cut at benchmarking
    Add some benchmark tests to compare PintArray performance against NumPy arrays of quantities.  Over time we should be able to show specific patterns where PintArrays confer substantial performance advantages over naive use of Quantities with Pandas.
    
    Signed-off-by: Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>

---
+++ pint_pandas/testsuite/benchmarks/test_30_numpy.py
@@ -0,0 +1,133 @@
+from typing import Generator, Any
+import itertools as it
+import operator
+
+import pytest
+
+import pint
+from pint.compat import np
+from pint.testsuite.helpers import requires_numpy
+
+from pint_pandas import PintArray
+
+
+SMALL_VEC_LEN = 3
+MID_VEC_LEN = 1_000
+LARGE_VEC_LEN = 1_000_000
+
+LENGTHS = ("short", "mid")
+ALL_VALUES = tuple(
+    f"{a}_{b}" for a, b in it.product(LENGTHS, ("list", "tuple", "array"))
+)
+ALL_ARRAYS = ("short_array", "mid_array")
+ALL_PINTARRAYS = ("short_PintArray", "mid_PintArray")
+UNITS = ("meter", "kilometer")
+
+OP1 = (operator.neg,)  # operator.truth,
+OP2_CMP = (operator.eq, operator.lt)
+OP2_MATH = (operator.add, operator.sub, operator.mul, operator.truediv)
+
+if np is None:
+    NUMPY_OP1_MATH = NUMPY_OP2_CMP = NUMPY_OP2_MATH = ()
+else:
+    NUMPY_OP1_MATH = (np.sqrt, np.square)
+    NUMPY_OP2_CMP = (np.equal, np.less)
+    NUMPY_OP2_MATH = (np.add, np.subtract, np.multiply, np.true_divide)
+
+
+def float_range(n: int) -> Generator[float, None, None]:
+    return (float(x) for x in range(1, n + 1))
+
+
+@pytest.fixture
+def setup(registry_tiny) -> tuple[pint.UnitRegistry, dict[str, Any]]:
+    data = {}
+    short = list(float_range(3))
+    mid = list(float_range(1_000))
+
+    data["short_list"] = short
+    data["short_tuple"] = tuple(short)
+    data["short_array"] = np.asarray(short)
+    data["mid_list"] = mid
+    data["mid_tuple"] = tuple(mid)
+    data["mid_array"] = np.asarray(mid)
+
+    ureg = registry_tiny
+
+    for key in ALL_ARRAYS:
+        data[key + "_meter"] = data[key] * ureg.meter
+        data[key + "_kilometer"] = data[key] * ureg.kilometer
+
+    data["short_PintArray"] = data["short_array"]
+    data["mid_PintArray"] = data["mid_array"]
+
+    for key in ALL_PINTARRAYS:
+        length, _ = key.split("_", 1)
+        data[key + "_meter"] = PintArray(data[f"{length}_array"], ureg.meter)
+        data[key + "_kilometer"] = PintArray(data[f"{length}_array"], ureg.kilometer)
+
+    return ureg, data
+
+
+@requires_numpy
+def test_finding_meter_getattr(benchmark, setup):
+    ureg, _ = setup
+    benchmark(getattr, ureg, "meter")
+
+
+# @requires_numpy
+# def test_finding_meter_getitem(benchmark, setup):
+#     ureg, _ = setup
+#     benchmark(operator.getitem, ureg, "meter")
+
+
+# @requires_numpy
+# @pytest.mark.parametrize(
+#     "unit", ["meter", "angstrom", "meter/second", "angstrom/minute"]
+# )
+# def test_base_units(benchmark, setup, unit):
+#     ureg, _ = setup
+#     benchmark(ureg.get_base_units, unit)
+
+
+@requires_numpy
+@pytest.mark.parametrize("key", ALL_ARRAYS)
+def test_build_array_by_mul(benchmark, setup, key):
+    ureg, data = setup
+    benchmark(operator.mul, data[key], ureg.meter)
+
+
+@requires_numpy
+@pytest.mark.parametrize("key", ALL_PINTARRAYS)
+def test_build_PintArray_by_mul(benchmark, setup, key):
+    ureg, data = setup
+    benchmark(operator.mul, data[key], ureg.meter)
+
+
+@requires_numpy
+@pytest.mark.parametrize(
+    "keys",
+    (
+        ("short_array_meter", "short_array_meter"),
+        ("short_array_meter", "short_array_kilometer"),
+        ("short_array_kilometer", "short_array_meter"),
+        ("short_array_kilometer", "short_array_kilometer"),
+        ("mid_array_meter", "mid_array_meter"),
+        ("mid_array_meter", "mid_array_kilometer"),
+        ("mid_array_kilometer", "mid_array_meter"),
+        ("mid_array_kilometer", "mid_array_kilometer"),
+        ("short_PintArray_meter", "short_PintArray_meter"),
+        ("short_PintArray_meter", "short_PintArray_kilometer"),
+        ("short_PintArray_kilometer", "short_PintArray_meter"),
+        ("short_PintArray_kilometer", "short_PintArray_kilometer"),
+        ("mid_PintArray_meter", "mid_PintArray_meter"),
+        ("mid_PintArray_meter", "mid_PintArray_kilometer"),
+        ("mid_PintArray_kilometer", "mid_PintArray_meter"),
+        ("mid_PintArray_kilometer", "mid_PintArray_kilometer"),
+    ),
+)
+@pytest.mark.parametrize("op", OP2_MATH + OP2_CMP + NUMPY_OP2_MATH + NUMPY_OP2_CMP)
+def test_op2(benchmark, setup, keys, op):
+    _, data = setup
+    key1, key2 = keys
+    benchmark(op, data[key1], data[key2])
